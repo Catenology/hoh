f71789c521b262184e6406bb93599cef59039846
Cherry Wang <v-cherrw@microsoft.com>
2016-08-04 15:39:23 -0700

    updated build process

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,11 @@ const rename = require('gulp-rename');
 const exec = require('child_process').exec;
 const replace = require('gulp-replace');
 const merge = require('merge-stream');
-const imagemin = require('gulp-imagemin');
+const cleancss = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const download = require('gulp-download');
 
 let deployargs = minimist(process.argv.slice(2));
 let conn = ftp.create({
@@ -24,7 +28,7 @@ let timestamp = Math.round(Date.now() / 1000);
 gulp.task('default', ['cachebust', 'zip']);
 
 gulp.task('clean', () => {
-    return del(['dist']);
+    return del(['dist', 'src/css/_vendor', 'src/css/fonts', 'src/js/_vendor', 'src/css/vendor.min.css', 'src/js/vendor.min.js']);
 });
 
 gulp.task('zip', ['build'], () => {
@@ -34,15 +38,63 @@ gulp.task('zip', ['build'], () => {
     return fszip;
 });
 
-gulp.task('build', ['clean'], (cb) => {
+gulp.task('build', ['scripts'], (cb) => {
     //jekyll build the site
     exec(['jekyll b --source src --destination dist/_site'], function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
-    })
+    });
 });
 
+//compile stylesheet
+gulp.task('styles', ['download'], () => {
+  let vendor = gulp.src(['src/css/_vendor/*.css'])
+  .pipe(cleancss())
+  .pipe(concat('vendor.min.css'))
+  .pipe(gulp.dest('src/css'));
+  let main = gulp.src(['src/css/*.css'])
+  .pipe(cleancss())
+  .pipe(concat('main.min.css'))
+  .pipe(gulp.dest('src/css'));
+  return merge(vendor, main);
+});
+
+//compile javascript
+gulp.task('scripts', ['styles'], () => {
+  let vendor = gulp.src(['src/js/_vendor/*.js'])
+  .pipe(uglify())
+  .pipe(concat('vendor.min.js'))
+  .pipe(gulp.dest('src/js'));
+
+  let main = gulp.src(['src/js/_main.js'])
+  .pipe(babel({
+      presets: ['es2015']
+  }))
+  .pipe(uglify())
+  .pipe(rename('main.min.js'))
+  .pipe(gulp.dest('src/js'));
+
+  return merge(vendor, main);
+});
+
+gulp.task('download', ['clean'], () => {
+    //catfw
+    let catfwcss = download('http://catfw.catenology.com/files/catfw.min.css')
+        .pipe(gulp.dest('src/css/_vendor/'));
+
+    let catfwfonts = download(['http://catfw.catenology.com/files/fonts/catif.ttf', 'http://catfw.catenology.com/files/fonts/catif.woff', 'http://catfw.catenology.com/files/fonts/catif.eot', 'http://catfw.catenology.com/files/fonts/catif.svg'])
+        .pipe(gulp.dest('src/css/fonts'));
+
+    let catfwjs = download('http://catfw.catenology.com/files/catfw.min.js')
+        .pipe(gulp.dest('src/js/_vendor/'));
+
+    //jquery
+    let jquery = download('https://code.jquery.com/jquery-3.1.0.min.js')
+        .pipe(gulp.dest('src/js/_vendor/'));
+
+    return merge(catfwcss, catfwjs, jquery);
+});
 //add timestamp to static assets to bust cache
 gulp.task('cachebust', ['build'], () => {
     let fscachebust = gulp.src(['dist/_site/**/*.html', 'dist/_site/**/*.md', 'dist/_site/**/*.markdown'])
@@ -53,8 +105,10 @@ gulp.task('cachebust', ['build'], () => {
 
 //ftp deployment
 gulp.task('deploy', ['cleanremote'], () => {
-  let globs = ['dist/_site/**/*.*'];
-  let fsdeploy = gulp.src(globs, {buffer: false})
+    let globs = ['dist/_site/**/*.*'];
+    let fsdeploy = gulp.src(globs, {
+            buffer: false
+        })
         .pipe(conn.dest('.'));
     return fsdeploy;
 })
@@ -67,7 +121,7 @@ gulp.task('cleanremote', (cb) => {
 });
 
 gulp.task('imagemin', () => {
-  gulp.src(['src/images/*','_ux/*.{png,jpeg,svg}'])
-  .pipe(imagemin())
-  .pipe(gulp.dest('src/images'));
+    gulp.src(['src/images/*', '_ux/*.{png,jpeg,svg}'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('src/images'));
 });
